1d05ff6c8c797e04040adcbdd16b4de5da8a38b6
sam99-web <keishsam99@gmail.com>
2026-04-07 18:59:56 +0200

    add element

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that returns elements and their lengths."""
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return a list of tuples with each element and its length."""
+    return [(i, len(i)) for i in lst]
