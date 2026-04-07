d0c8f1c947ab2dc98570fcae49d81c53f1f5e104
sam99-web <keishsam99@gmail.com>
2026-04-07 18:00:51 +0200

    add contact

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of str1 and str2."""
+    return str1 + str2
