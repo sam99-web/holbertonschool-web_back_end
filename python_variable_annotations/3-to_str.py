a74e180a0404cb6afbced0dd226a871ca801f7f8
sam99-web <keishsam99@gmail.com>
2026-04-07 18:17:43 +0200

    annotation str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that converts a float to a string."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a float n."""
+    return str(n)
