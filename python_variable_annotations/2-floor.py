1c2daa86e0c83de11a67b96b4991eddb015e2a80
sam99-web <keishsam99@gmail.com>
2026-04-07 18:14:43 +0200

    add float annotation

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that computes the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float n."""
+    import math
+    return math.floor(n)
