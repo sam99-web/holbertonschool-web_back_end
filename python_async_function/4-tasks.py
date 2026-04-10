55a6a535002c3e1cb9000bc63e9f9fe7c1b7f8e8
sam99-web <keishsam99@gmail.com>
2026-04-11 01:24:41 +0200

    Add task_wait_n

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawne task_wait_random n fois et retourne les delais en ordre croissant."""
+    """Spawne task_wait_random n fois et retourne les delais tries."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
     for task in asyncio.as_completed(tasks):
