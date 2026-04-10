9b4d185226bbc068de6818aa38fa8122bb3f38d3
sam99-web <keishsam99@gmail.com>
2026-04-11 00:35:52 +0200

    Add basic async coroutine

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module contenant une coroutine asynchrone d'attente aléatoire."""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+   
+   
+  
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
