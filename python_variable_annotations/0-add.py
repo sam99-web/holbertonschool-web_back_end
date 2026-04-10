8db8d9e14ab474906253627d5c51cc66b4da9ed0
sam99-web <keishsam99@gmail.com>
2026-04-10 23:58:42 +0200

    modif file add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-"""Module that adds two floats."""
+"""Module contenant une fonction d'addition de nombres flottants."""
 
 
 def add(a: float, b: float) -> float:
-    """Return the sum of a and b."""
+    """Additionne deux flottants et retourne leur somme.
+
+    Args:
+        a: Le premier nombre flottant.
+        b: Le deuxième nombre flottant.
+
+    Returns:
+        La somme de a et b sous forme de float.
+    """
     return a + b
