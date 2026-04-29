597f5c8bb050fce7cb999a0ae2159b8f4c6fec1e
sam99-web <keishsam99@gmail.com>
2026-04-29 17:33:22 +0200

    Task 10 - for...of loop

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+
+  for (const value of array) {
+    array[i] = appendString + value;
+    i++;
+  }
+
+  return array;
+}
+
