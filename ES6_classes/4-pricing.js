a2bc4fa911d11541fe676392759e151eed268011
sam99-web <keishsam99@gmail.com>
2026-05-01 15:22:16 +0200

    task 4 princing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // AMOUNT
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // CURRENCY
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  // METHOD
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // STATIC METHOD
+  static convertPrice(amount, conversionRate) {
+  return amount * conversionRate;
+}
+}
+
